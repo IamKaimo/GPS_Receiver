9051bad8d7940de3e8899fd2f4245502e77f4cce
ahmedmohamed986 <ahmed.mohamed98611@gmail.com>
2021-06-05 22:54:24 +0200

    LCD Driver

---
+++ LCD.h
@@ -0,0 +1,10 @@
+#ifndef LCD_H
+#define LCD_H
+
+void Delay(long);
+void lcd_init (void);
+void display_data (char* data);
+void lcd_send_cmd(char cmd);
+void lcd_set_cursor(char row, char col);
+
+#endif
